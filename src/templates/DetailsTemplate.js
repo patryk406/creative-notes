3483da4627179ef03b15812f4b16ff8dbf83e4ec
patryk406 <49455624+patryk406@users.noreply.github.com>
2021-10-12 22:20:11 +0200

    setting single note view

--- src/templates/DetailsTemplate.js
+++ src/templates/DetailsTemplate.js
@@ -1,15 +1,90 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import UserPageTemplate from "./UserPageTemplate";
 
-const DetailsTemplate = ({ children, pageType }) => {
+import UserPageTemplate from "./UserPageTemplate";
+import Heading from "components/atoms/Heading/Heading";
+import Paragraph from "components/atoms/Paragraph/Paragraph";
+import Button from "components/atoms/Button/Button";
+
+import OsmaIcon from "assets/avatars/Osma.png";
+
+const StyledDetailWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  height: 90vh;
+  width: 80%;
+`;
+const StyledHeadingWrapper = styled.div`
+  position: absolute;
+  top: 15%;
+  left: 15%;
+`;
+const StyledContent = styled.div`
+  padding: 10rem 0 0 0;
+  width: 30%;
+  text-align: center;
+`;
+const StyledIcon = styled.img`
+  height: 8rem;
+  width: 8rem;
+  border-radius: 5rem;
+  border: 0.3rem ${({ theme, activeColor }) => theme[activeColor]} solid;
+  position: absolute;
+  top: 20%;
+  right: 20%;
+`;
+const StyledButton = styled(Button)`
+  text-align: center;
+  color: black;
+  text-decoration: none;
+  line-height: 3rem;
+  font-weight: bold;
+`;
+const DetailsTemplate = ({
+  pageType,
+  title,
+  content,
+  id,
+  created,
+  travelName,
+}) => {
   return (
     <UserPageTemplate pageType={pageType}>
-      {children}
-      <Link to="/">Back</Link>
+      <StyledDetailWrapper>
+        <StyledHeadingWrapper>
+          <Heading>{title}</Heading>
+          <Paragraph>
+            {pageType}, {created}
+          </Paragraph>
+        </StyledHeadingWrapper>
+        <StyledIcon src={OsmaIcon} activeColor={pageType} />
+        <StyledContent>{content}</StyledContent>
+        <StyledButton as={Link} to="/">
+          back/save
+        </StyledButton>
+      </StyledDetailWrapper>
     </UserPageTemplate>
   );
 };
 
+DetailsTemplate.propTypes = {
+  pageType: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  created: PropTypes.string,
+  travelName: PropTypes.string,
+};
+DetailsTemplate.defaultProps = {
+  pageType: "",
+  title: "",
+  content: "",
+  id: "",
+  created: "",
+  travelName: "",
+};
 export default DetailsTemplate;
--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -15,13 +15,29 @@ const DetailsPage = ({ match }) => {
       case routes.creative:
         setPageType("creatives");
         break;
+      default:
+        console.log("something went wrong");
     }
   }, [match.path]);
 
+  const dummyTravel = {
+    id: 5,
+    title: "Stars",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lobortis mi mollis aliquet fringilla. Sed pulvinar vel ex sed facilisis. Vestibulum eu nunc sed sem lacinia tempus. Phasellus ac condimentum purus. Sed fermentum venenatis nisl. Morbi hendrerit justo sollicitudin erat aliquet sodales. Vivamus augue augue, mattis eget nunc et, ultricies suscipit elit. Aliquam sed condimentum odio. Nulla elit est, sodales sodales eleifend at, congue sed eros. Duis at sagittis massa.",
+    created: `${new Date().toLocaleDateString()}`,
+    travelName: "Osma",
+  };
   return (
-    <DetailsTemplate pageType={pageType}>
-      <>{pageType}</>
-    </DetailsTemplate>
+    <DetailsTemplate
+      pageType={pageType}
+      title={dummyTravel.title}
+      content={dummyTravel.content}
+      id={dummyTravel.id}
+      created={dummyTravel.created}
+      travelName={dummyTravel.travelName}
+      key={dummyTravel.id}
+    />
   );
 };
 
